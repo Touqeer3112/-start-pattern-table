9f3080c8cd203a389ad419af945334fa6abb85b7
Muhammad Touqeer <31.tmouqeer@gmail.com>
2024-09-23 07:59:31 -0700

    start pattern

---
+++ app.js
@@ -0,0 +1,41 @@
+ var userValue =prompt("Enter the number")
+
+ for (let i = 1; i <=userValue; i++){
+    for(let j = 1;j <= i; j++){
+        document.write(j + " ");
+    }
+    document.write("<br>");
+ }
+
+
+ var userValue =prompt("Enter the second number")
+
+ for (let i = 1; i <=userValue; i++){
+    for(let j = 1;j <= i; j++){
+        document.write(  " * ");
+    }
+    document.write("<br>");
+ }
+//  for (var i = 1 ; i <= userValue ; i++){
+//     var pattern ="";
+//     for (var j =1 ; j <=i ; j++){
+//         pattern = pattern + "*";
+          
+//     }
+//  }
+//  iprintf("Enter the value number")
+//  main()
+// {
+//     inti,j;
+//     for(i=1;i<=5;i++)
+//     {
+//         for(j=1;j<=i;j++)
+//         {
+//             printf("*")
+//         }
+//         printf("\n")
+//     }
+// }
+
+
+
